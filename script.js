0932a5ef148dc11c35db7816ae78979801eccd3a
ippatev <alexanderippatiev24@gmail.com>
2023-04-28 01:49:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log(" _    _      _ _         ");
+console.log("| |  | |    | | |        ");
+console.log("| |__| | ___| | | ___   ");
+console.log("|  __  |/ _ \\ | |/ _ \\  ");
+console.log("| |  | |  __/ | | (_) | ");
+console.log("|_|  |_|\\___|_|_|\\___/  ");
